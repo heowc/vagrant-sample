7bff9873946a8db127ddea8caf71c09c68250ee7
heowc <heowc1992@gmail.com>
2018-02-21 13:33:07 +0900

    Fix: [ubuntu_terraform] 스크립트 수정 - args 추가

--- ubuntu1604_terraform/provisioning/terraform.sh
+++ ubuntu1604_terraform/provisioning/terraform.sh
@@ -1,7 +1,11 @@
-TERRAFORM_VERSION='0.11.3'
+#!/usr/bin/env bash
+echo '============================ TERRAFORM SETTING ============================'
+
+TERRAFORM_VERSION=$1
 
 sudo wget https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
 sudo unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+sudo chmod 755 terraform
 sudo mv terraform /usr/local/bin/
 
 terraform --version
